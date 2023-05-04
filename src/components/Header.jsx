e2555a87a1743b47dd66ca6fc6e40288e10a68fd
muntasir-fahim <fa3877957@gmail.com>
2023-05-05 01:03:04 +0600

    project initialized

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      {/* desktop & tab */}
+      <div className="hidden md:flex w-full h-full  "></div>
+
+      {/* mobile */}
+      <div className="flex md:hidden w-full h-full "></div>
+    </div>
+  );
+};
+
+export default Header;
