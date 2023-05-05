62343b9c0452e550509085f19e5d025e1f5ac006
muntasir-fahim <fa3877957@gmail.com>
2023-05-06 01:19:15 +0600

    hero section initialized

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="App w-screen h-auto flex flex-col bg-primary">
         <Header />
 
-        <main className="mt-24 p-8 w-full">
+        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
           <Routes>
             <Route path="/" element={<MainContainer />} />
             <Route path="/createitem" element={<CreateItem />} />
